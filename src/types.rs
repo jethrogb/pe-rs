5b8436ec2d9ffe19565a9a5462e89e672f5c24f5
Michael McGee <mcgee@dropbox.com>
2017-06-02 12:17:40 -0700

    Add DebugDirectory directory entry type

--- src/types.rs
+++ src/types.rs
@@ -329,6 +329,20 @@ unsafe impl RefSafe for ExportDirectory {}
 
 #[repr(packed)]
 #[derive(Clone, Debug)]
+pub struct DebugDirectory {
+    pub characteristics: u32,
+    pub time_date_stamp: u32,
+    pub major_version: u16,
+    pub minor_version: u16,
+    pub debug_type: u32,
+    pub size_of_data: u32,
+    pub address_of_raw_data: RVA<()>,
+    pub pointer_to_raw_data: FP<()>,
+}
+unsafe impl RefSafe for DebugDirectory {}
+
+#[repr(packed)]
+#[derive(Clone, Debug)]
 pub struct RawExportAddress(pub RVA<()>);
 unsafe impl RefSafe for RawExportAddress {}
 
