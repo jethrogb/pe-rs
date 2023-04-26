47b7f670798c71799dc267a1ed89a29dc7e78bd9
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-04-26 11:19:10 -0700

    rm deprecated macro `try`

--- src/lib.rs
+++ src/lib.rs
@@ -123,31 +123,31 @@ impl<'data> Pe<'data> {
 
 	fn resolve_rva<T>(&self, rva: RVA<T>) -> Result<FP<T>> {
 		let length=size_of::<T>() as u32;
-		Ok(try!(self.resolve_rva_raw(rva+0u32,length,None)).offset(0))
+		Ok(self.resolve_rva_raw(rva+0u32,length,None)?.offset(0))
 	}
 
 	fn resolve_rva_slice<T>(&self, rva: RVA<[T]>, count: u32) -> Result<FP<[T]>> {
 		let length=size_of::<T>() as u32*count;
-		Ok(try!(self.resolve_rva_raw(rva+0u32,length,None)).offset(0))
+		Ok(self.resolve_rva_raw(rva+0u32,length,None)?.offset(0))
 	}
 
 // PUBLIC
 	pub fn new(data: &'data [u8]) -> Result<Pe<'data>> {
-		let sig=*try!(data.ref_at(FP::<u16>::new(0)));
+		let sig=*data.ref_at(FP::<u16>::new(0))?;
 		let pe_header_fp=if sig==DOS_SIGNATURE {
-			try!(data.ref_at(FP::<DosHeader>::new(0))).new
+			data.ref_at(FP::<DosHeader>::new(0))?.new
 		} else if sig as u32==PE_SIGNATURE {
 			FP::new(0)
 		} else {
 			return Err(Error::NotPe)
 		};
-		let pe_header=try!(data.ref_at(pe_header_fp));
+		let pe_header=data.ref_at(pe_header_fp)?;
 
 		if pe_header.size_of_optional_header<2 {
 			return Err(Error::NotPe);
 		}
 		let pe_oh_fp=pe_header_fp+(size_of::<PeHeader>() as u32);
-		let sig: u16=*try!(data.ref_at(pe_oh_fp.offset(0)));
+		let sig: u16=*data.ref_at(pe_oh_fp.offset(0))?;
 
 		let pe_dd_fp;
 		let dd_size;
@@ -157,14 +157,14 @@ impl<'data> Pe<'data> {
 				if pe_header.size_of_optional_header<s { return Err(Error::NotPe) }
 				dd_size=pe_header.size_of_optional_header-s;
 				pe_dd_fp=pe_oh_fp.offset(s as u32);
-				PeOptionalHeader::Pe32(try!(data.ref_at(pe_oh_fp.offset(0))))
+				PeOptionalHeader::Pe32(data.ref_at(pe_oh_fp.offset(0))?)
 			},
 			OH_SIGNATURE_PE32P => {
 				let s=size_of::<PeOptionalHeader64>() as u16;
 				if pe_header.size_of_optional_header<s { return Err(Error::NotPe) }
 				dd_size=pe_header.size_of_optional_header-s;
 				pe_dd_fp=pe_oh_fp.offset(s as u32);
-				PeOptionalHeader::Pe32Plus(try!(data.ref_at(pe_oh_fp.offset(0))))
+				PeOptionalHeader::Pe32Plus(data.ref_at(pe_oh_fp.offset(0))?)
 			},
 			_ => return Err(Error::NotPe),
 		};
@@ -173,27 +173,27 @@ impl<'data> Pe<'data> {
 		if ((n*size_of::<DataDirectory<u32>>() as u32) as u16)>dd_size {
 			return Err(Error::InvalidSize);
 		}
-		let pe_dd=try!(data.ref_slice_at(pe_dd_fp,n));
+		let pe_dd=data.ref_slice_at(pe_dd_fp,n)?;
 
 		let pe_sec_fp=pe_dd_fp.offset(n*(size_of::<DataDirectory<u32>>() as u32));
-		let pe_sec=try!(data.ref_slice_at(pe_sec_fp,pe_header.number_of_sections as u32));
+		let pe_sec=data.ref_slice_at(pe_sec_fp,pe_header.number_of_sections as u32)?;
 
 		Ok(Pe{data:data,h:pe_header,oh:pe_oh,directories:pe_dd,sections:pe_sec})
 	}
 
 	pub fn ref_at<T: RefSafe>(&self, rva: RVA<T>) -> Result<&'data T> {
-		let fp=try!(self.resolve_rva(rva));
+		let fp=self.resolve_rva(rva)?;
 		self.data.ref_at(fp)
 	}
 
 	pub fn ref_slice_at<T: RefSafe>(&self, rva: RVA<[T]>, count: u32) -> Result<&'data [T]> {
-		let fp=try!(self.resolve_rva_slice(rva,count));
+		let fp=self.resolve_rva_slice(rva,count)?;
 		self.data.ref_slice_at(fp,count)
 	}
 
 	pub fn ref_cstr_at(&self, rva: RVA<[CChar]>) -> Result<&'data [CChar]> {
 		let mut max_len=0;
-		let fp=try!(self.resolve_rva_raw(rva+0u32,0,Some(&mut max_len)));
+		let fp=self.resolve_rva_raw(rva+0u32,0,Some(&mut max_len))?;
 		self.data.ref_cstr_at(fp.offset(0),Some(max_len))
 	}
 
@@ -242,15 +242,15 @@ impl<'data> Pe<'data> {
 	}
 
 	pub fn get_exports(&self) -> Result<Exports> {
-		let ddir=try!(self.get_directory::<ExportDirectory>());
+		let ddir=self.get_directory::<ExportDirectory>()?;
 		if (ddir.size as usize)<size_of::<ExportDirectory>() {
 			return Err(Error::InvalidSize);
 		}
-		Ok(Exports{pe:self,ddir:ddir,edir:try!(self.ref_at(ddir.virtual_address))})
+		Ok(Exports{pe:self,ddir:ddir,edir:self.ref_at(ddir.virtual_address)?})
 	}
 
 	pub fn get_relocations<'pe>(&'pe self) -> Result<RelocationIter<'pe,'data>> {
-		let ddir=try!(self.get_directory::<RelocationBlock>());
+		let ddir=self.get_directory::<RelocationBlock>()?;
 		Ok(RelocationIter{pe:self,next_rblock:ddir.virtual_address,end:ddir.virtual_address+ddir.size})
 	}
 }
@@ -286,19 +286,19 @@ impl<'pe,'data: 'pe> Exports<'pe, 'data> {
 	}
 
 	pub fn lookup_symbol(&self, symbol: &str) -> Result<ExportAddress<'data>> {
-		let pos=try!(try!(self.get_names()).iter().position(|&name_rva|{
+		let pos=self.get_names()?.iter().position(|&name_rva|{
 			self.pe.ref_cstr_at(name_rva).ok().map_or(false,|cstr|cstr.as_os_str()==symbol)
-		}).ok_or(Error::SymbolNotFound));
-		let ordinal_offset=try!(self.get_ordinal_offsets())[pos];
-		let export=try!(try!(self.get_export_addresses()).get(ordinal_offset as usize).ok_or(Error::ExportNotFound));
+		}).ok_or(Error::SymbolNotFound)?;
+		let ordinal_offset=self.get_ordinal_offsets()?[pos];
+		let export=self.get_export_addresses()?.get(ordinal_offset as usize).ok_or(Error::ExportNotFound)?;
 		Ok(self.concretize_export_address(export))
 	}
 }
 
 impl<'pe,'data: 'pe> RelocationIter<'pe,'data> {
 	fn advance(&mut self) -> Result<(RVA<()>,&'data [Relocation])> {
-		let rblock=try!(self.pe.ref_at(self.next_rblock));
-		let relocs: &[Relocation]=try!(self.pe.ref_slice_at(self.next_rblock.offset(size_of::<RelocationBlock>() as u32),rblock.block_size/2));
+		let rblock=self.pe.ref_at(self.next_rblock)?;
+		let relocs: &[Relocation]=self.pe.ref_slice_at(self.next_rblock.offset(size_of::<RelocationBlock>() as u32),rblock.block_size/2)?;
 		self.next_rblock=self.next_rblock.offset(rblock.block_size);
 		Ok((rblock.page_rva,relocs))
 	}
